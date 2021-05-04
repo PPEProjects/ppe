9c9dc6963e590823bc67af5701d0dedab4c3eb31
sitd <tdisivip98@gmail.com>
2021-05-04 13:19:44 +0700

    fix hien thi ngon ngu

--- ppe_fe_admin/src/pages/jobs/JobsPage.js
+++ ppe_fe_admin/src/pages/jobs/JobsPage.js
@@ -41,7 +41,6 @@ const JobsPage = () => {
         <div className="grid grid-cols-12 gap-4 mx-6 ">
           <div className="col-span-12 flex items-center justify-between mt-6 ">
             <h1 className="text-xl font-bold">Jobs</h1>
-            <Language />
           </div>
           <Filter />
 
