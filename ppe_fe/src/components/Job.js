846a8497d95a96b9319c875d32fadc44e55c6eb2
sitd <tdisivip98@gmail.com>
2021-05-10 09:56:53 +0700

    fix loi hien thi job

--- ppe_fe/src/components/Job.js
+++ ppe_fe/src/components/Job.js
@@ -74,12 +74,12 @@ class Job extends Component {
                   <section className="">
                     <h1 className="text-3xl">{job.title}</h1>
                     <div className="mt-2">
-                        <a
+                        <p
                           href="#"
                           className="uppercase text-sm border px-2 py-1 text-gray-700 hover:border-red-600 hover:text-red-700"
                         >
-                         <span className="truncate">{job?.more?.skills}</span>
-                        </a>
+                         <span className="">{job?.more?.skills}</span>
+                        </p>
                     </div>
                     <div className="">
                       <button
